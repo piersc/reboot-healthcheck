a15cd7291ab958b4602372591c8dc4ae4637c319
Piers Cornwell <piersc@users.noreply.github.com>
2017-09-15 17:23:31 +0100

    Create reboot-healthcheck.py

---
+++ reboot-healthcheck.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import os
+import subprocess
+
+
+def run_command(cmd):
+    """Run executable and return output"""
+
+    try:
+        proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except OSError, e:
+        print("Error running {}: {}".format(cmd, e))
+
+    return proc.stdout.readlines()
+
+def get_listening_services():
+    """Return set of networking listening services"""
+
+    netstat = run_command(['/bin/netstat', '-tlnp'])
+    listening_services = set()
+
+    for line in netstat:
+        if line.startswith('tcp'):
+            process = line.split()[6]
+            # Renamne e.g. nimbus(cdm) to nimbus
+            if "(" in process:
+                process = process.split('(')[0]
+            if process != "-":
+                listening_services.add(process.split('/')[1])
+
+    return listening_services
+
+def get_systemd_enabled():
+    """Return list of systemctl enabled service units"""
+
+    systemd_enabled = []
+
+    if os.path.isfile('/bin/systemctl'):
+        output = run_command(['systemctl', 'list-unit-files'])
+
+        for unit in output:
+            if "service" in unit and "enabled" in unit:
+                 systemd_enabled.append(unit.split()[0].split('.service')[0])
+
+    return systemd_enabled
+
+def get_sysv_enabled():
+    """Return list of sysv enabled services"""
+
+    sysv_enabled = []
+
+    if os.path.isfile('/sbin/chkconfig'):
+        output = run_command(['chkconfig', '--list'])
+
+        for line in output:
+            if "3:on" in line:
+                sysv_enabled.append(line.split()[0])
+    
+    return sysv_enabled
+
+def compare_listening_to_enabled(listening, enabled):
+    """Return list of services the are listening but not enabled"""
+
+    service_map = {"cupsd": "cups", "systemd-resolv": "systemd-resolved", "master": "postfix"}
+
+    for l in listening:
+        if l in service_map:
+            l = service_map[l]
+        if l in enabled:
+            print("OK! {0} running and enabled".format(l))
+        else:
+            print("CHECK! {0} running but not enabled".format(l))
+
+listening_services = get_listening_services()
+systemd_enabled = get_systemd_enabled()
+sysv_enabled = get_sysv_enabled()
+
+compare_listening_to_enabled(listening_services, systemd_enabled + sysv_enabled)
