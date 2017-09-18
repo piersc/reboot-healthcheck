5c65f05eef99d7112c6310987589add8abb0d600
Piers Cornwell <piers.cornwell@rackspace.co.uk>
2017-09-18 10:36:43 +0100

    Module loading

--- reboot-healthcheck.py
+++ reboot-healthcheck.py
@@ -1,80 +1,23 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-import os
-import subprocess
+from os import listdir
+from os.path import isfile, join
+import importlib
+import sys
 
 
-def run_command(cmd):
-    """Run executable and return output"""
+def load_modules():
+    """Import (i.e. run) all modules, and return list of them"""
 
-    try:
-        proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    except OSError, e:
-        print("Error running {}: {}".format(cmd, e))
+    sys.path.append("./modules")
+    modules = [f.split(".py")[0] for f in listdir("modules") if isfile(join("modules", f))]
+    return [importlib.import_module(m) for m in set(modules) if not "__init__" in m and not m.startswith(".")]
 
-    return proc.stdout.readlines()
+def list_modules(modules):
+    """Print module name and version"""
+    print("\nRan these modules:")
+    [print("- {0} {1}".format(m.__name__, m.__version__)) for m in modules]
 
-def get_listening_services():
-    """Return set of networking listening services"""
-
-    netstat = run_command(['/bin/netstat', '-tlnp'])
-    listening_services = set()
-
-    for line in netstat:
-        if line.startswith('tcp'):
-            process = line.split()[6]
-            # Renamne e.g. nimbus(cdm) to nimbus
-            if "(" in process:
-                process = process.split('(')[0]
-            if process != "-":
-                listening_services.add(process.split('/')[1])
-
-    return listening_services
-
-def get_systemd_enabled():
-    """Return list of systemctl enabled service units"""
-
-    systemd_enabled = []
-
-    if os.path.isfile('/bin/systemctl'):
-        output = run_command(['systemctl', 'list-unit-files'])
-
-        for unit in output:
-            if "service" in unit and "enabled" in unit:
-                 systemd_enabled.append(unit.split()[0].split('.service')[0])
-
-    return systemd_enabled
-
-def get_sysv_enabled():
-    """Return list of sysv enabled services"""
-
-    sysv_enabled = []
-
-    if os.path.isfile('/sbin/chkconfig'):
-        output = run_command(['chkconfig', '--list'])
-
-        for line in output:
-            if "3:on" in line:
-                sysv_enabled.append(line.split()[0])
-    
-    return sysv_enabled
-
-def compare_listening_to_enabled(listening, enabled):
-    """Return list of services the are listening but not enabled"""
-
-    service_map = {"cupsd": "cups", "systemd-resolv": "systemd-resolved", "master": "postfix"}
-
-    for l in listening:
-        if l in service_map:
-            l = service_map[l]
-        if l in enabled:
-            print("OK! {0} running and enabled".format(l))
-        else:
-            print("CHECK! {0} running but not enabled".format(l))
-
-listening_services = get_listening_services()
-systemd_enabled = get_systemd_enabled()
-sysv_enabled = get_sysv_enabled()
-
-compare_listening_to_enabled(listening_services, systemd_enabled + sysv_enabled)
+plugins = load_modules()
+list_modules(plugins)
